7c5ff22c137c8fc7677fb64283cd8ace65b88cd1
william koller <wkoller25@gmail.com>
2021-03-02 01:21:12 -0300

    fix: adjuested UserSchema

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -17,10 +17,10 @@ export class User {
   @Prop({ type: String, required: true })
   password: string;
 
-  @Prop({ type: Date })
+  @Prop({ type: Date, default: Date.now })
   created_at: Date;
 
-  @Prop({ type: Date })
+  @Prop({ type: Date, default: Date.now })
   updated_at: Date;
 }
 
