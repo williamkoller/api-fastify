54527f0afeb1229d02345fcbb03a506a54f9df55
william koller <wkoller25@gmail.com>
2021-03-02 01:52:52 -0300

    chore: update USerSchema

--- src/user/schemas/user.schema.ts
+++ src/user/schemas/user.schema.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
-@Schema()
+@Schema({ timestamps: true })
 export class User {
   @Prop({ type: String, required: true })
   name: string;
@@ -16,12 +16,6 @@ export class User {
 
   @Prop({ type: String, required: true })
   password: string;
-
-  @Prop({ type: Date, default: Date.now })
-  created_at: Date;
-
-  @Prop({ type: Date, default: Date.now })
-  updated_at: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
