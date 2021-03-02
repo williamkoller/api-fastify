83efaf67a8f9cc846b1adab6cdd440935a126f52
william koller <wkoller25@gmail.com>
2021-03-01 22:05:45 -0300

    feat: add setup fastify

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from '@/app.module';
+
+const logger = new Logger('Main');
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+  await app.listen(process.env.PORT, () => logger.log('0.0.0.0'));
+}
+bootstrap();
