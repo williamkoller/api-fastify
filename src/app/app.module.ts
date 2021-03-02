1c3c4a90e4fdee707cd77a8d6015103de1a61e6a
william koller <wkoller25@gmail.com>
2021-03-02 00:38:49 -0300

    chore: update config mongoose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from '@/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -8,7 +8,13 @@ import { MongooseModule } from '@nestjs/mongoose';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.MONGO_URL, { useUnifiedTopology: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
     forwardRef(() => UserModule),
   ],
 })
