f7c2bcffb1e1c31d837876a745d24fff25a3e793
william koller <wkoller25@gmail.com>
2021-03-05 00:01:29 -0300

    feat: add JwtAuth in loadUserById

--- src/user/controllers/load-user-by-id/load-user-by-id.controller.ts
+++ src/user/controllers/load-user-by-id/load-user-by-id.controller.ts
@@ -1,13 +1,15 @@
 import { User } from '@/user/schemas/user.schema';
 import { LoadUserByIdService } from '@/user/services/load-user-by-id/load-user-by-id.service';
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { JwtAuth } from '@/auth/guard/jwt-auth/jwt-auth.guard';
 
 @ApiTags('user')
 @Controller('user')
 export class LoadUserByIdController {
   constructor(private readonly loadUserByIdService: LoadUserByIdService) {}
 
+  @UseGuards(JwtAuth)
   @Get(':id')
   async loadUserById(@Param('id') id: string): Promise<User> {
     return await this.loadUserByIdService.loadById(id);
